ff1ba2eed79c4bb8a9526924f91ddaa9934dfa03
Tshamz <tylershambora@Tshamz-MacBook-Pro.local>
2015-08-18 00:13:35 -0700

    commented code

--- worker.js
+++ worker.js
@@ -93,3 +93,105 @@ function getUri(data) {
 
 getHtml();
 //authorizeCode();
+
+
+
+
+
+
+// var $ = require('cheerio');
+// var restler = require('restler');
+// var moment = require('moment');
+// var Promise = require('promise');
+// var SpotifyWebApi = require('spotify-web-api-node');
+// var config = require('./config.json');
+
+// var spotifyApi = new SpotifyWebApi(config.credentials);
+
+// var code = 'AQDLAb_jmc5WGkLcxKqgehBcxPZ61RaxMJawjiqVmOrO28HP3QCbdNrc_5bcILLLCqa6bDGvnA8l6CET0_rWAoHAi_QWOylnYpxsxqYAYANUtd3g175uRppG9Keg8IekpNOoXtOs0ywbluh7Hrus7LuUBYNQZh_mcg_fWMgwdKLNISJw7qSweWfBPJ2xWRtV095bC1PWJxUyhMUYsv9mQZ1-V_kc0h1lnVVIQbkCf9IhearzdVI3IlVzUT9DL5BnWFboNR4KFwurNMtAcpRw2uFJrT5Ls0pg5g';
+
+// var refreshToken = 'AQAPWph1ifSfvOgvkQiFrBMps5TqFayVrV0cdBQfsYRXTMrBRqzVaazuretG3UW-VADyYDTuWXk0HDYtVTHaQnfjsyFLws6eTLCT77H4fW3H85P3ydkrQH7GyX-PH3EBZYI';
+
+// var createPlaylist = function(marketplaceData) {
+//   var playlistTitle = 'Music from ' + marketplaceData.date;
+//   // Retrieve an access token and a refresh token
+// spotifyApi.authorizationCodeGrant(code)
+//   .then(function(data) {
+//     var accessToken = data.body.access_token;
+//     var refreshToken = data.body.refresh_token;
+//     console.log('The access token is ', accessToken);
+//     console.log('The refresh token is ', refreshToken);
+//     console.log('The token expires in ', data.body.expires_in);
+
+//     spotifyApi.setAccessToken(data.body.access_token);
+//     spotifyApi.setRefreshToken(data.body.refresh_token);
+//     return spotifyApi.getUserPlaylists('tshamz');
+//   })
+//   .then(function(data) {
+//     console.log(data.body.items);
+//   });
+    // .then(function(data) {
+    //   console.log('Ok. Playlist created!');
+    //   var playlistId = data.body.id;
+    //   var trackIds = [];
+    //   marketplaceData.songs.forEach(function(song, index) {
+
+
+    //   });
+    //   return
+    // });
+// }();
+
+
+// function getSpotifyUris(trackData) {
+//   return Promise.all(trackData.map(function(track, index) {
+//     var spotifyUri;
+//     spotifyApi.searchTracks()
+//       .then(function(data) {
+//         var results = data.body.tracks.items;
+//         for (var i = 0; i < results.length; i++) {
+//           if (results[i].name.toLowerCase().indexOf(song.title.toLowerCase() > -1)) {
+//             for (var n = 0; n < results[i].artists.length; n++) {
+//               if (results[i].artists[n].name.toLowerCase().indexOf(song.artist.toLowerCase() > -1)) {
+//                 track.uri = results[i].uri;
+//                 break resultsLoop;
+//               }
+//             }
+//           }
+//         }
+//       });
+//   });
+//   return track;
+// }
+
+// spotifyApi.addTracksToPlaylist('tshamz', playlistId, results[i].uri);
+
+// function getSpotifyTrackIds(trackData) {
+//   spotifyApi.searchTracks(song.title + ' ' + song.artist)
+//     .then(function(data) {
+//       var results = data.body.tracks.items;
+
+//       for (var i = 0; i < results.length; i++) {
+//         if (results[i].name.toLowerCase().indexOf(song.title.toLowerCase() > -1)) {
+//           artistsLoop:
+//           for (var n = 0; n < results[i].artists.length; n++) {
+//             if (results[i].artists[n].name.toLowerCase().indexOf(song.artist.toLowerCase() > -1)) {
+//               console.log(results[i].uri);
+
+//               break resultsLoop;
+//             }
+//           }
+//         }
+//       }
+//     });
+
+// }
+
+
+// getMusicData();
+
+
+// var searchForTrackId = function(title, artist) {
+
+// };
+
