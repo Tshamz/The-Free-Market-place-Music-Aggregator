4825337cb3e97eeb574aec2d1e5b6c89d6c5920a
Tyler Shambora <tyler@theshamboras.com>
2018-06-01 13:01:44 -0700

    remove whitespaces

--- worker.js
+++ worker.js
@@ -3,8 +3,6 @@ const cheerio       = require('cheerio');
 const fetch         = require('node-fetch');
 const SpotifyWebApi = require('spotify-web-api-node');
 
-
-
 const now = moment();
 const then = moment().subtract(1, 'week');
 
